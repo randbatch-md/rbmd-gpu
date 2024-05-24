ab7c8731d22a257584b7fe961b3f58c086fde151
huan.liu <huanliu_uestc@163.com>
2024-05-24 16:17:06 +0800

    build tools

--- cmake/build_tool.cmake
+++ cmake/build_tool.cmake
@@ -1,23 +1,28 @@
 macro(build_tool)
-	set(TOOL_INSTALL_PATH ${tool_path}/${CMAKE_SYSTEM_NAME}/${CMAKE_BUILD_TYPE}/filename_without_ext)	
+	set(TOOL_INSTALL_PATH ${tooldir}/${CMAKE_SYSTEM_NAME}/${CMAKE_BUILD_TYPE}/${toolname_without_ext})	
 	#avoid repeated build
-	#if(NOT EXISTS ${TOOL_INSTALL_PATH})
-		message("--- tar -zxvf ${filename} ---")
+	if(NOT EXISTS ${TOOL_INSTALL_PATH})
+		message("****** build ${toolname_without_ext} start ******")
+
+		message("--- tar -zxvf ${toolname} ---")
 		execute_process(COMMAND ${CMAKE_COMMAND}
 		-E tar -xzvf ${tool_path}
 		WORKING_DIRECTORY ${PROJECT_BINARY_DIR})
 
-		message("--- cmake -S . -B build ${filename} ---")
-		set(tool_source ${PROJECT_BINARY_DIR}/${filename_without_ext})
+		message("--- cmake -S . -B build ${toolname_without_ext} ---")
+		set(tool_source ${PROJECT_BINARY_DIR}/${toolname_without_ext})
 		execute_process(COMMAND ${CMAKE_COMMAND} -S ${tool_source} -B ${tool_source}/build
 		-DCMAKE_INSTALL_PREFIX=${TOOL_INSTALL_PATH} -DBUILD_SHARED_LIBS=false)
 
-		message("--- cmake --build build ${filename} ---")
+		message("--- cmake --build build ${toolname_without_ext} ---")
 		execute_process(COMMAND ${CMAKE_COMMAND} --build ${tool_source}/build
 		--config ${CMAKE_BUILD_TYPE})
 
-		message("--- cmake --install ${filename} ---")
+		message("--- cmake --install ${toolname_without_ext} ---")
 		execute_process(COMMAND ${CMAKE_COMMAND} --install ${tool_source}/build
 		--config ${CMAKE_BUILD_TYPE})
-	#endif()
+
+		message("****** build ${toolname_without_ext} end ******")
+
+	endif()
 endmacro()
