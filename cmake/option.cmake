6b46da8101231937683b3a081a66e92d42f81871
huan.liu <huanliu_uestc@163.com>
2024-05-24 11:43:44 +0800

    配置double/float int/longlong选项

---
+++ cmake/option.cmake
@@ -0,0 +1,2 @@
+option(ENABLE_DOUBLE "Enable double, default float" OFF)
+option(ENABLE_64BIT_IDS "Enable long long, default int" OFF)
--- src/model/Types.h
+++ src/model/Types.h
@@ -2,9 +2,17 @@
 
 namespace rbmd
 {
+#ifdef USE_DOUBLE
+	using Real = double;
+#else
 	using Real = float;
+#endif
 
+#ifdef USE_LONGLONG
+	using Id = long long;
+#else
 	using Id = int;
+#endif
 
 	using Range = Real[3][2];
 
