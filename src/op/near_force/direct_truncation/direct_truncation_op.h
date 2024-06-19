a56f0546fa76019a2c7ceb5123ff5b33e8a7fa74
huan.liu <huanliu_uestc@163.com>
2024-06-19 17:15:04 +0800

    solve run error

--- src/op/near_force/direct_truncation/direct_truncation_op.h
+++ src/op/near_force/direct_truncation/direct_truncation_op.h
@@ -9,4 +9,6 @@ struct direct_truncation_op
 	void operator()();
 };
 
+extern template struct direct_truncation_op<float, DEVICE_GPU>;
+extern template struct direct_truncation_op<double, DEVICE_GPU>;
 }
