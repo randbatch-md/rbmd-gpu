8d90402c216176e2fba4a470ccb5b6ed03d415f5
huan.liu <huanliu_uestc@163.com>
2024-06-19 16:00:11 +0800

    enchancement run error

--- src/op/near_force/direct_truncation/rocm/direct_truncation_op.hip.cu
+++ src/op/near_force/direct_truncation/rocm/direct_truncation_op.hip.cu
@@ -1,12 +1,15 @@
 #include "base/device_types.h""
 #include "near_force/direct_truncation/direct_truncation_op.h"
 #include <iostream>
+#include <hip/hip_runtime.h>
 
 namespace op
 {
 
+#define THREADS_PER_BLOCK 256
+
 template <typename FPTYPE>
-__device__ __inline__
+__global__
 void test()
 {
 	std::cout << "device::test()" << std::endl;
@@ -15,7 +18,9 @@ void test()
 template<typename FPTYPE>
 void direct_truncation_op<FPTYPE, DEVICE_GPU>::operator()()
 {
-	test();
+	int ng = 1;
+	int block = (ng + THREADS_PER_BLOCK - 1) / THREADS_PER_BLOCK;
+	hipLaunchKernelGGL(test, dim3(block), dim3(THREADS_PER_BLOCK),0,0);
 }
 
 template struct direct_truncation_op<float, DEVICE_GPU>;
