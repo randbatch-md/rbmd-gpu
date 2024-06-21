dd3549caae9bfc3f48cea54fd34dc34add282597
huan.liu <huanliu_uestc@163.com>
2024-06-21 08:56:31 +0800

    slove make error

--- src/op/near_force/direct_truncation/direct_truncation_op.cpp
+++ src/op/near_force/direct_truncation/direct_truncation_op.cpp
@@ -9,8 +9,8 @@ namespace op
 	{
 		std::cout << "CPU LJ" << std::endl;
 	}
-	template LJ<float>();
-	template LJ<double>();
+	template void LJ<float>();
+	template void LJ<double>();
 
 	//template <typename FPTYPE>
 	//struct direct_truncation_op<FPTYPE, device::DEVICE_CPU>
