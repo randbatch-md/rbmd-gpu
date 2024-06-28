95252de0860e2668e602224cf0796c87d4454ab2
huan.liu <huanliu_uestc@163.com>
2024-06-28 11:30:56 +0800

    LJ

--- src/system/md_system.cpp
+++ src/system/md_system.cpp
@@ -14,11 +14,11 @@ int MDSystem::Evolve()
 	
 	rbmd::Real dt = 0.5, fmt2v = 1.0;
 	rbmd::Real3* force = new rbmd::Real3[nAtoms];
-	op::resize_memory_op(d_dt, 1);
-	op::resize_memory_op(d_fmt2v, 1);
-	op::resize_memory_op(d_mass, potential_data._mass.size());
-	op::resize_memory_op(d_v, nAtoms);
-	op::resize_memory_op(d_force, nAtoms);
+	op::resize_memory_op<rbmd::Real>(d_dt, 1);
+	op::resize_memory_op<rbmd::Real>(d_fmt2v, 1);
+	op::resize_memory_op<rbmd::Real>(d_mass, potential_data._mass.size());
+	op::resize_memory_op<rbmd::Real3>(d_v, nAtoms);
+	op::resize_memory_op<rbmd::Real3>(d_force, nAtoms);
 
 	op::sync_memory_h2d_op<rbmd::Real, device::DEVICE_GPU>(&dt, &d_dt, 1);
 	op::sync_memory_h2d_op<rbmd::Real, device::DEVICE_GPU>(&fmt2v, &d_fmt2v, 1);
