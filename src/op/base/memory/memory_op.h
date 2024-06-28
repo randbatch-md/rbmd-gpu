5258aca4d0048b1dd1b2596203778a23711eb3b2
huan.liu <huanliu_uestc@163.com>
2024-06-28 09:52:05 +0800

    memory op

--- src/op/base/memory/memory_op.h
+++ src/op/base/memory/memory_op.h
@@ -1,30 +1,30 @@
-//#pragma once
-//#include "base/device_types.h"
-//#include "types.h"
-//
-//namespace op {
-//
-//	template <typename FPTYPE, typename DEVICE>
-//	struct delete_memory_op
-//	{
-//		void operator()(FPTYPE* arr);
-//	};
-//
-//	template <typename FPTYPE, typename DEVICE>
-//	struct resize_memory_op
-//	{
-//		void operator()(FPTYPE*& arr, const size_t size);
-//	};
-//
-//	template <typename FPTYPE, typename DEVICE>
-//	struct sync_memory_h2d_op
-//	{
-//		void operator()(const FPTYPE* arr_s, FPTYPE arr_d, const size_t size);
-//	};
-//
-//	template <typename FPTYPE, typename DEVICE>
-//	struct sync_memory_d2h_op
-//	{
-//		void operator()(const FPTYPE* arr_s, FPTYPE arr_d, const size_t size);
-//	};
-// }
+#pragma once
+#include "base/device_types.h"
+#include "types.h"
+
+namespace op {
+
+	template <typename FPTYPE, typename DEVICE>
+	struct delete_memory_op
+	{
+		void operator()(FPTYPE* arr);
+	};
+
+	template <typename FPTYPE, typename DEVICE>
+	struct resize_memory_op
+	{
+		void operator()(FPTYPE*& arr, const size_t size);
+	};
+
+	template <typename FPTYPE, typename DEVICE>
+	struct sync_memory_h2d_op
+	{
+		void operator()(const FPTYPE* arr_s, FPTYPE arr_d, const size_t size);
+	};
+
+	template <typename FPTYPE, typename DEVICE>
+	struct sync_memory_d2h_op
+	{
+		void operator()(const FPTYPE* arr_s, FPTYPE arr_d, const size_t size);
+	};
+ }
