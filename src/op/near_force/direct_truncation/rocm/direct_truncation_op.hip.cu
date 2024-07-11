056fdf17b292706b643eeb97049f68e2209df0db
LH <huanliu_uestc@163.com>
2024-07-11 16:33:14 +0800

    test

--- src/op/near_force/direct_truncation/rocm/direct_truncation_op.hip.cu
+++ src/op/near_force/direct_truncation/rocm/direct_truncation_op.hip.cu
@@ -14,10 +14,10 @@ void ComputeCellId(
 	const rbmd::Real3& right,
 	const rbmd::Id3& dim)
 {
-	__shared__ auto dxdydz = (right - left) / dim; //should be shared memory
-	cellids.data[0] = (position.data[0] - left.data[0]) / dxdydz.data[0];
-	cellids.data[1] = (position.data[1] - left.data[1]) / dxdydz.data[1];
-	cellids.data[2] = (position.data[2] - left.data[2]) / dxdydz.data[2];
+	//__shared__ auto dxdydz = (right - left) / dim; //should be shared memory
+	//cellids.data[0] = (position.data[0] - left.data[0]) / dxdydz.data[0];
+	//cellids.data[1] = (position.data[1] - left.data[1]) / dxdydz.data[1];
+	//cellids.data[2] = (position.data[2] - left.data[2]) / dxdydz.data[2];
 }
 
 template <typename FPTYPE>
