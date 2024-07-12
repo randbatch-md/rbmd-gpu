fcab2b7593076282d16a1dba300c676631ad1ebf
LH <huanliu_uestc@163.com>
2024-07-12 14:03:22 +0800

    test

--- src/op/near_force/direct_truncation/rocm/direct_truncation_op.hip.cu
+++ src/op/near_force/direct_truncation/rocm/direct_truncation_op.hip.cu
@@ -14,18 +14,19 @@ void ComputeCellId(
 	rbmd::Real3 right,
 	rbmd::Id3 dim)
 {
-	printf("test\n");
-	//printf("right: %f,%f,%f\n", right.data[0], right.data[1], right.data[2]);
-	//printf("left:%f,%f,%f\n", left.data[0], left.data[1], left.data[2]);
-	//printf("dim: %d,%d,%d\n", dim.data[0], dim.data[1], dim.data[2]);
+	printf("-----ComputeCellId-----\n");
+	printf("right: %f,%f,%f\n", right.data[0], right.data[1], right.data[2]);
+	printf("left:%f,%f,%f\n", left.data[0], left.data[1], left.data[2]);
+	printf("dim: %d,%d,%d\n", dim.data[0], dim.data[1], dim.data[2]);
 
-	//rbmd::Real3 dxdydz = (right - left) / dim; //should be shared memory
-	//printf("dxdydz: %f,%f,%f\n", dxdydz.data[0], dxdydz.data[1], dxdydz.data[2]);
+	rbmd::Real3 dxdydz = (right - left) / dim; //should be shared memory
+	printf("dxdydz: %f,%f,%f\n", dxdydz.data[0], dxdydz.data[1], dxdydz.data[2]);
 
-	//cellids.data[0] = (position.data[0] - left.data[0]) / dxdydz.data[0];
-	//cellids.data[1] = (position.data[1] - left.data[1]) / dxdydz.data[1];
-	//cellids.data[2] = (position.data[2] - left.data[2]) / dxdydz.data[2];
-	//printf("dim: %d,%d,%d", cellids.data[0], cellids.data[1], cellids.data[2]);
+	cellids.data[0] = (position.data[0] - left.data[0]) / dxdydz.data[0];
+	cellids.data[1] = (position.data[1] - left.data[1]) / dxdydz.data[1];
+	cellids.data[2] = (position.data[2] - left.data[2]) / dxdydz.data[2];
+	printf("dim: %d,%d,%d", cellids.data[0], cellids.data[1], cellids.data[2]);
+	printf("------------------------\n");
 
 }
 
