5d62d9e86ee1765ab13297f3085a196c18ae09b8
LH <huanliu_uestc@163.com>
2024-08-13 14:23:49 +0800

    MemoryScheduler

--- src/data_manager/memory_scheduler/memory_scheduler.h
+++ src/data_manager/memory_scheduler/memory_scheduler.h
@@ -4,6 +4,15 @@
 class MemoryScheduler : public Object
 {
 public:
+	/**
+	 * @brief async memeory host to device
+	 * @return error code
+	*/
 	virtual int asyncMemoryH2D() = 0;
+
+	/**
+	 * @brief async memory device to host
+	 * @return error code
+	*/
 	virtual int asyncMemoryD2H() = 0;
 };
