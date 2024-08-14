81bbbae4233583a2e4848ec618b6dddd494c950b
Qinaiwen <iamcavey@163.com>
2024-08-14 11:35:33 +0800

    拉取新代码

--- src/ensemble/ensembles/NVTensemble.cpp
+++ src/ensemble/ensembles/NVTensemble.cpp
@@ -11,9 +11,25 @@ NVTensemble::NVTensemble()
 
 void NVTensemble::Init()
 {
-	_position_controller->Init();
-	_velocity_controller->Init();
-	_force_controller->Init();
+	if (_para.GetParameter<std::string>(PARA_FILE_TYPE) == "EAM")
+	{
+		_potential_file.open(_para.GetParameter<std::string>(PARA_POTENTIAL_FILE));
+		ReadPotentialFile(_potential_file);
+		InitStyle();
+	}
+
+	ExecutionMD::Init();
+
+	InitialCondition();
+
+	ComputeForce();
+
+	// 速度和位置的初始化 包括了 _positiong  velocity的初始化，以及相互之间的初始化；还有charge、mass、atoms_id、_molecule_id、_Volume 等 计算他们是各自需要的参数
+	_position_controller.Init(); 
+	_velocity_controller.Init();
+
+	// 力的初始化 包括不同力场的文件读取(如EAM势能文件)、近远程力的初始参数填充和计算（如远程：_psamplekey）
+	_force_controller.Init(); 
 
 
 }
