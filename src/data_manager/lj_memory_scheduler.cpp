769ce340fbd42d298ecbe9ec57da6658a4af115d
LH <huanliu_uestc@163.com>
2024-08-16 17:26:25 +0800

    asyncMemoryH2D

--- src/data_manager/lj_memory_scheduler.cpp
+++ src/data_manager/lj_memory_scheduler.cpp
@@ -1,18 +1,20 @@
-#include "./include/lj_memory_scheduler.h"
-
-//const int lj_num_streams = 7
+#include "include/lj_memory_scheduler.h"
+#include "include/data_manager.h"
+#include "include/device_data.h"
+#include "include/md_data.h"
 
 bool LJMemoryScheduler::asyncMemoryH2D()
 {
-	//hipStream_t streams[lj_num_streams];
+	auto& data_manager = DataManager::getInstance();
+	auto& device_data = data_manager.getDeviceData();
+	auto& md_data = data_manager.getMDData();
+	auto& num_atoms = md_data->_structure_info_data->_num_atoms;
+	auto& structure_data = md_data->_structure_data;
+	auto& h_px = structure_data->_h_px;
 
+	device_data->_d_px.resize(num_atoms);
+	thrust::copy(h_px, h_px + num_atoms, device_data->_d_px);
 
-
-
-	//for (int i = 0; i < lj_num_streams; ++i)
-	//{
-	//	hipStreamSynchronize(streams[i]);
-	//}
 	return true;
 }
 
