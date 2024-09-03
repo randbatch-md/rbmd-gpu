2e290573804438618b3e193d6424a8ec88684b80
Qinaiwen <iamcavey@163.com>
2024-09-03 14:46:56 +0800

    vs编译通过

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,4 @@
-#include "md_application.h"
+#include "application/md_application.h"
 
 int main(int argc, char*argv[])
 {
@@ -7,5 +7,3 @@ int main(int argc, char*argv[])
 
 	return 0;
 }
-// 注释：“工具”->选项->“doxygen” 修改为doxygen 、*；
-// 使用：Ctrl+/ 详细注释；“///" 单个注释
