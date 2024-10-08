8d471d1a3ae100823c61ff1e2ad200134ec9eeef
罗茂瑛 <956149202@qq.com>
2024-10-08 16:16:47 +0800

    add charge data

--- src/reader/src/atomic_reader.cpp
+++ src/reader/src/atomic_reader.cpp
@@ -57,17 +57,38 @@ int AtomicReader::ReadData()
 
 void AtomicReader::AllocateDataSpace()
 {
-	auto& md_data = DataManager::getInstance().getMDData();
-	auto& info = _md_data._structure_info_data;
-	auto& data = _md_data._structure_data;
-	HIP_CHECK(MALLOCHOST(&(data->_h_atoms_id), *(info->_num_atoms)*sizeof(rbmd::Id)));
-	HIP_CHECK(MALLOCHOST(&(data->_h_atoms_type), *(info->_num_atoms) * sizeof(rbmd::Id)));
-	HIP_CHECK(MALLOCHOST(&(data->_h_px), *(info->_num_atoms)*sizeof(rbmd::Id)));
-	HIP_CHECK(MALLOCHOST(&(data->_h_py), *(info->_num_atoms)*sizeof(rbmd::Id)));
-	HIP_CHECK(MALLOCHOST(&(data->_h_pz), *(info->_num_atoms)*sizeof(rbmd::Id)));
-	HIP_CHECK(MALLOCHOST(&(data->_h_vx), *(info->_num_atoms)*sizeof(rbmd::Id)));
-	HIP_CHECK(MALLOCHOST(&(data->_h_vy), *(info->_num_atoms)*sizeof(rbmd::Id)));
-	HIP_CHECK(MALLOCHOST(&(data->_h_vz), *(info->_num_atoms)*sizeof(rbmd::Id)));
+  auto atom_style = DataManager::getInstance().getConfigData()->Get<std::string>("atom_style", "init_configuration", "read_data");
+  if ("atomic" == atom_style)
+  {
+    auto& md_data = DataManager::getInstance().getMDData();
+    auto& info = _md_data._structure_info_data;
+    auto& data = _md_data._structure_data;
+    
+    HIP_CHECK(MALLOCHOST(&(data->_h_atoms_id), *(info->_num_atoms)*sizeof(rbmd::Id)));
+    HIP_CHECK(MALLOCHOST(&(data->_h_atoms_type), *(info->_num_atoms) * sizeof(rbmd::Id)));
+    HIP_CHECK(MALLOCHOST(&(data->_h_px), *(info->_num_atoms)*sizeof(rbmd::Id)));
+    HIP_CHECK(MALLOCHOST(&(data->_h_py), *(info->_num_atoms)*sizeof(rbmd::Id)));
+    HIP_CHECK(MALLOCHOST(&(data->_h_pz), *(info->_num_atoms)*sizeof(rbmd::Id)));
+    HIP_CHECK(MALLOCHOST(&(data->_h_vx), *(info->_num_atoms)*sizeof(rbmd::Id)));
+    HIP_CHECK(MALLOCHOST(&(data->_h_vy), *(info->_num_atoms)*sizeof(rbmd::Id)));
+    HIP_CHECK(MALLOCHOST(&(data->_h_vz), *(info->_num_atoms)*sizeof(rbmd::Id)));
+
+  }else if ("charge" == atom_style)
+  {
+    auto& md_data = DataManager::getInstance().getMDData();
+    auto& info = _md_data._structure_info_data;
+    auto& charge_structure_data = _md_data._structure_data;
+    ChargeStructureData* data = dynamic_cast<ChargeStructureData*>(charge_structure_data.get());
+    HIP_CHECK(MALLOCHOST(&(data->_h_atoms_id), *(info->_num_atoms)*sizeof(rbmd::Id)));
+    HIP_CHECK(MALLOCHOST(&(data->_h_atoms_type), *(info->_num_atoms) * sizeof(rbmd::Id)));
+    HIP_CHECK(MALLOCHOST(&(data->_h_px), *(info->_num_atoms)*sizeof(rbmd::Id)));
+    HIP_CHECK(MALLOCHOST(&(data->_h_py), *(info->_num_atoms)*sizeof(rbmd::Id)));
+    HIP_CHECK(MALLOCHOST(&(data->_h_pz), *(info->_num_atoms)*sizeof(rbmd::Id)));
+    HIP_CHECK(MALLOCHOST(&(data->_h_vx), *(info->_num_atoms)*sizeof(rbmd::Id)));
+    HIP_CHECK(MALLOCHOST(&(data->_h_vy), *(info->_num_atoms)*sizeof(rbmd::Id)));
+    HIP_CHECK(MALLOCHOST(&(data->_h_vz), *(info->_num_atoms)*sizeof(rbmd::Id)));
+    HIP_CHECK(MALLOCHOST(&(data->_h_charge), *(info->_num_atoms)*sizeof(rbmd::Id)));
+  }
 }
 
 int AtomicReader::ReadAtoms(const rbmd::Id& atoms_num)
@@ -80,7 +101,10 @@ int AtomicReader::ReadAtoms(const rbmd::Id& atoms_num)
 		rbmd::Id atom_type;
 
 		_line_start = &_mapped_memory[_locate];
-		for (auto num = 0; _locate < _file_size && num < atoms_num; ++_locate)
+	    auto atom_style = DataManager::getInstance().getConfigData()->Get<std::string>("atom_style", "init_configuration", "read_data");
+	    if ("atomic" == atom_style)
+	    {
+	    	for (auto num = 0; _locate < _file_size && num < atoms_num; ++_locate)
 		{
 			if (_mapped_memory[_locate] == '\n')
 			{
@@ -100,6 +124,31 @@ int AtomicReader::ReadAtoms(const rbmd::Id& atoms_num)
 				_line_start = &_mapped_memory[_locate];
 			}
 		}
+	    }
+	    else if ("charge" == atom_style)
+	    {
+			auto& charge_structure_data = _md_data._structure_data;
+			ChargeStructureData* data = dynamic_cast<ChargeStructureData*>(charge_structure_data.get());
+	    	for (auto num = 0; _locate < _file_size && num < atoms_num; ++_locate)
+		{
+			if (_mapped_memory[_locate] == '\n')
+			{
+				auto line = std::string(_line_start, &_mapped_memory[_locate]);
+				std::istringstream iss(line);
+				if (rbmd::IsLegalLine(line))
+				{
+					iss >> atom_id;
+					auto index = atom_id - 1;
+					ids[index] = atom_id;
+					iss >> types[index] >> data->_h_charge[index];
+					iss >> data->_h_px[index] >> data->_h_py[index] >> data->_h_pz[index];
+					++num;
+					//std::cout << atom_id << " " << types[index] << " " << data->_h_charge[index]  << " " << data->_h_px[index] << " " << data->_h_py[index] << " " << data->_h_pz[index] << std::endl;
+				}
+				_line_start = &_mapped_memory[_locate];
+			}
+		}
+	    }
 	}
 	catch (const std::exception& e)
 	{
