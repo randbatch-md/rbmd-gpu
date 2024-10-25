ff89e39432d1b684bdee9b2f84d0ec3fd0f51261
Qinaiwen <iamcavey@163.com>
2024-10-25 11:11:38 +0800

    rang修改 轨迹文件修改

--- src/output/src/TrajectoryOutput.cpp
+++ src/output/src/TrajectoryOutput.cpp
@@ -9,6 +9,7 @@
 #include "spdlog/spdlog.h"
 #include <spdlog/async.h>
 #include <spdlog/sinks/basic_file_sink.h>
+#include "box.h"
 
 TrajectoryOutput::TrajectoryOutput()
 : _interval(DataManager::getInstance().getConfigData()->Get<rbmd::Id>("interval", "outputs", "trajectory_out"))
@@ -35,24 +36,25 @@ void TrajectoryOutput::Execute()
             std::vector<rbmd::Real> h_py(_num_atoms);
             std::vector<rbmd::Real> h_pz(_num_atoms);
             std::vector<rbmd::Real> h_atoms_type(_num_atoms);
-            rbmd::Range h_rang;
+            rbmd::Real h_rang_xmin, h_rang_ymin, h_rang_zmin;
+            rbmd::Real h_rang_xmax, h_rang_ymax, h_rang_zmax;
+            Box h_box;
 
             thrust::copy(_device_data->_d_px.begin(), _device_data->_d_px.end(), h_px.begin());
             thrust::copy(_device_data->_d_py.begin(), _device_data->_d_py.end(), h_py.begin());
             thrust::copy(_device_data->_d_pz.begin(), _device_data->_d_pz.end(), h_pz.begin());
             thrust::copy(_device_data->_d_atoms_type.begin(), _device_data->_d_atoms_type.end(), h_atoms_type.begin());
-
-            CHECK_RUNTIME(MEMCPY(&h_rang, _structure_info_data->_range, sizeof(rbmd::Range), D2H));
-
-
+                      
+            CHECK_RUNTIME(MEMCPY(&h_box, (_device_data->_d_box), sizeof(Box), D2H));
+            
             spdlog::info("ITEM: TIMESTEP");
             spdlog::info("{}", test_current_step);
             spdlog::info("ITEM: NUMBER OF ATOMS");
             spdlog::info("{}", _num_atoms);
             spdlog::info("ITEM: BOX BOUNDS pp pp pp");
-            spdlog::info("{} {}", h_rang[0][0], h_rang[0][1]);
-            spdlog::info("{} {}", h_rang[1][0], h_rang[1][1]);
-            spdlog::info("{} {}", h_rang[2][0], h_rang[2][1]);
+            spdlog::info("{} {}", h_box._coord_min[0], h_box._coord_max[0]);
+            spdlog::info("{} {}", h_box._coord_min[1], h_box._coord_max[1]);
+            spdlog::info("{} {}", h_box._coord_min[2], h_box._coord_max[2]);
             spdlog::info("ITEM: ATOMS id type x y z");
 
             for (auto i = 0; i < _num_atoms; ++i)
--- src/simulate_pipeline/src/controller/position_controller/default_position_controller.cpp
+++ src/simulate_pipeline/src/controller/position_controller/default_position_controller.cpp
@@ -25,12 +25,7 @@ void DefaultPositionController::Update() {
       //_device_data->_shake_vz = _device_data->_d_pz;
     }
     op::UpdatePositionOp<device::DEVICE_GPU> update_position_op;
-    update_position_op(_num_atoms, _dt, (*_structure_info_data->_range)[0][0],
-                       (*_structure_info_data->_range)[1][0],
-                       (*_structure_info_data->_range)[2][0],
-                       (*_structure_info_data->_range)[0][1],
-                       (*_structure_info_data->_range)[1][1],
-                       (*_structure_info_data->_range)[2][1],
+    update_position_op(_num_atoms, _dt, _device_data->_d_box,
                        thrust::raw_pointer_cast(_device_data->_d_vx.data()),
                        thrust::raw_pointer_cast(_device_data->_d_vy.data()),
                        thrust::raw_pointer_cast(_device_data->_d_vz.data()),
@@ -40,12 +35,6 @@ void DefaultPositionController::Update() {
   } else {
     op::UpdatePositionFlagOp<device::DEVICE_GPU> update_position_op;
     update_position_op(_num_atoms, _dt, _device_data->_d_box,
-                       (*_structure_info_data->_range)[0][0],
-                       (*_structure_info_data->_range)[1][0],
-                       (*_structure_info_data->_range)[2][0],
-                       (*_structure_info_data->_range)[0][1],
-                       (*_structure_info_data->_range)[1][1],
-                       (*_structure_info_data->_range)[2][1],
                        thrust::raw_pointer_cast(_device_data->_d_vx.data()),
                        thrust::raw_pointer_cast(_device_data->_d_vy.data()),
                        thrust::raw_pointer_cast(_device_data->_d_vz.data()),
