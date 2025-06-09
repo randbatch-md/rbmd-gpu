910af5303221ff49a121ed714cd36d2aeca121ab
zengkehan <792316664@qq.com>
2025-06-09 14:18:56 +0800

    fullrun

---
+++ src/data_manager/include/force_field/mace_force_field_data.h
@@ -0,0 +1,31 @@
+#pragma once
+
+#include "common/types.h"
+#include "force_field_data.h"
+
+class CVFFForceFieldData : public ForceFieldData {
+ public:
+  bool checkForceField() const override { return true; }
+
+  /// mass
+  rbmd::Real* _h_mass;
+
+  /// eps
+  rbmd::Real* _h_eps;
+
+  /// sigma
+  rbmd::Real* _h_sigma;
+
+  /// bond
+  rbmd::Real* _h_bond_coeffs_k;
+  rbmd::Real* _h_bond_coeffs_equilibrium;
+
+  /// angle
+  rbmd::Real* _h_angle_coeffs_k;
+  rbmd::Real* _h_angle_coeffs_equilibrium;
+
+  /// dihedral
+  rbmd::Real* _h_dihedral_coeffs_k;
+  rbmd::Real* _h_dihedral_coeffs_sign;
+  rbmd::Real* _h_dihedral_coeffs_multiplicity;
+};
