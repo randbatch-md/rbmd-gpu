820bd1ef1cef6b19aa78f1cd17af1f9364ccdf6d
gyf <70243570@qq.com>
2025-06-23 14:17:51 +0800

    Fix memory leaks in Reader and remove invalid code.

--- src/common/types.h
+++ src/common/types.h
@@ -14,12 +14,4 @@ using Int = long long;
 using Id = int;
 // using Int = int;
 #endif
-
-/// Real[0][0]: min x
-/// Real[0][1]: max x
-/// Real[1][0]: min y
-/// Real[1][1]: max y
-/// Real[2][0]: min z
-/// Real[2][1]: max z
-using Range = Real[3][2];
 }  // namespace rbmd
